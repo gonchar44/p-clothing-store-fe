65fcb990c0af2a44a3dbdda2c453bf71520b2799
Artem Honcharenko <artemgonchar7@gmail.com>
2024-07-17 12:33:05 +0200

    feat: deleted Link common component & refactored NotfoundPage

--- src/pages/NotfoundPage/NotfoundPage.tsx
+++ src/pages/NotfoundPage/NotfoundPage.tsx
@@ -1,15 +1,20 @@
-import { Link } from '@components'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@gonchar44/react-components-pack'
 
-export const NotfoundPage = () => (
-  <div className="w-1/2 py-40 mx-auto">
-    <section className=" font-bold text-center">
-      <h2 className="text-h2 text-secondary-error">404</h2>
+export const NotfoundPage = () => {
+  const navigate = useNavigate()
 
-      <p className="mb-5">
-        Sorry! The page you are looking for cannot be found... 🥲
-      </p>
+  return (
+    <div className="w-1/2 py-40 mx-auto">
+      <section className=" font-bold text-center">
+        <h2 className="text-h2 text-secondary-error">404</h2>
 
-      <Link text="Go Back" path="/" />
-    </section>
-  </div>
-)
+        <p className="mb-5">
+          Sorry! The page you are looking for cannot be found... 🥲
+        </p>
+
+        <Button onClick={() => navigate('/')}>Go Back</Button>
+      </section>
+    </div>
+  )
+}
